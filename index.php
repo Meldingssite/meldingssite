92f5ce55077a6d6f0d9c57f7325a5f80eadacb69
Joep Kant <joepkant@outlook.com>
2018-02-14 13:26:05 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+            function writeline(text){
+        document.write(text + "<br><br>");
+        };
+        function writeh1(text){
+            document.write("<h1>" + text + "</h1>");
+        };
+        
+        writeh1("This is an example.");
+    </script>
+</body>
+</html>
