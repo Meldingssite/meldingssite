ec18bdebc409dbfabec5beede024982026918c6b
Damion de Wit <damion.dewit@outlook.com>
2018-02-19 15:18:48 +0100

    Made some PHP automation

--- index.php
+++ index.php
@@ -10,8 +10,51 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
 </head>
 <body>
+
+<!-- PHP Classes -->
+<?php
+class MainButton {
+    public $innerText;
+    public $imageUrl;
+}
+?>
+
+<!-- PHP Variables -->
 <?php
     $image_dir = "./resources/images/";
+?>
+
+<!-- PHP Objects -->
+<?php
+    $mainBtns = [];
+
+    for($i = 0; $i < 8; $i++){
+        $mainBtns[$i] = new MainButton;
+    }
+
+    $mainBtns[0]->innerText = "Ongeval";
+    $mainBtns[0]->imageUrl = $image_dir."category-FirstAid.png";
+
+    $mainBtns[1]->innerText = "Vechtpartij";
+    $mainBtns[1]->imageUrl = $image_dir."category-fight.png";
+    
+    $mainBtns[2]->innerText = "Wapens";
+    $mainBtns[2]->imageUrl = $image_dir."category-weapons.png";
+    
+    $mainBtns[3]->innerText = "Drugs";
+    $mainBtns[3]->imageUrl = $image_dir."category-drugs.png";
+    
+    $mainBtns[4]->innerText = "Diefstal";
+    $mainBtns[4]->imageUrl = $image_dir."category-theft.png";
+    
+    $mainBtns[5]->innerText = "Pesten";
+    $mainBtns[5]->imageUrl = $image_dir."category-bullying.png";
+    
+    $mainBtns[6]->innerText = "Overlast";
+    $mainBtns[6]->imageUrl = $image_dir."category-nuisance.png";
+    
+    $mainBtns[7]->innerText = "Overige";
+    $mainBtns[7]->imageUrl = $image_dir."category-other.png";
 ?>
 
     <header>
@@ -20,16 +63,14 @@
     
     <main>
         <div class="box">Wat wilt u melden</div>
-        
-        <div class="btn">Ongeval</div>
-        <div class="btn">Vechtpartij</div>
-        <div class="btn">Wapens</div>
-        <div class="btn">Drugs</div>
-        <div class="btn">Diefstal</div>
-        <div class="btn">Pesten</div>
-        <div class="btn">Overlast</div>
-        <div class="btn">Overige</div>
-        
+
+    <?php foreach($mainBtns as $button) { ?>    
+        <div class="btn">
+            <img src="<?= $button->imageUrl?>" alt="">    
+            <?= $button->innerText ?>
+        </div>
+    <?php } ?>
+
         
     </main>
 </body>
