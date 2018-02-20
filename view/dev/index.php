92f8084208496e7dd5987cd7652ae334b4c0de05
Damion de Wit <damion.dewit@outlook.com>
2018-02-20 13:48:23 +0100

    Started forms styling

---
+++ view/dev/index.php
@@ -0,0 +1,23 @@
+<header>
+    <h1>Development Page</h1>
+</header>
+
+<main>
+
+    <form action="">
+        
+        <!-- Tekstveld -->
+        <label for="where">Waar is het gebeurd?</label>
+        <input type="text" name="where">
+
+        <fieldset>
+            <legend>Is het slachtoffer bewusteloos?</legend>
+            <!-- Radio Buttons -->
+            <input type="radio" name="unconscious" value="Ja">
+            <input type="radio" name="unconscious" value="Nee">
+
+        </fieldset>
+    </form>
+
+
+</main>
