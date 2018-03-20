e1630cab5323f786d7a1f1802d50e9a6e0f01304
Unknown <vincent1906@live.nl>
2018-03-20 14:02:30 +0100

    Dynamische radio buttons en text input via JSON

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -3,6 +3,6 @@
 var SCRIPT_DIR = <?php echo json_encode(URL . SCRIPT_DIR) ?>;
 var IMAGE_DIR = <?php echo json_encode(URL . IMAGE_DIR) ?>;
 </script>
-<script src="<?php echo URL . SCRIPT_DIR?>.js"></script>
+<script src="<?php echo URL . SCRIPT_DIR?>ajaxtest.js"></script>
 </body>
 </html>
