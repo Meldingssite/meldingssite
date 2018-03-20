d2f5e6e20def77890f12c8bc3c2ed9bcc1db3609
Unknown <vincent1906@live.nl>
2018-03-20 10:45:57 +0100

    Better styling on dev page

--- view/dev/index.php
+++ view/dev/index.php
@@ -9,14 +9,18 @@
         <fieldset>
             <!-- Tekstveld -->
             <legend>Waar is het gebeurd?</legend>
+            <section class=input>
             <input type="text" name="where" placeholder="Locatie">
+            </section>
         </fieldset>
 
         <fieldset>
             <legend>Is het slachtoffer bewusteloos?</legend>
             <!-- Radio Buttons -->
-            <input type="radio" name="unconscious" value="Ja">
-            <input type="radio" name="unconscious" value="Nee">
+            <section class=input>
+            Ja: <input type="radio" name="unconscious" value="Ja">
+            Nee: <input type="radio" name="unconscious" value="Nee">
+            </section>
 
         </fieldset>
     </form>
