d816c61a07b320e0c1fd518088c466cad75f377e
Damion de Wit <damion.dewit@outlook.com>
2018-03-21 16:58:44 +0100

    Moved javascript from footer to end of view/home/index.php

--- view/home/index.php
+++ view/home/index.php
@@ -10,3 +10,10 @@
 
         
 </main>
+
+<script>
+// Import PHP variables into JavaScript
+var SCRIPT_DIR = <?php echo json_encode(URL . SCRIPT_DIR) ?>;
+var IMAGE_DIR = <?php echo json_encode(URL . IMAGE_DIR) ?>;
+</script>
+<script src="<?php echo URL . SCRIPT_DIR?>ajaxtest.js"></script>
