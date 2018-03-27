ef191cf361e079c4af208ba8e7e1047770b70117
Damion de Wit <damion.dewit@outlook.com>
2018-03-27 18:09:40 +0200

    Fixed issue with saving JSON

--- model/AdminModel.php
+++ model/AdminModel.php
@@ -5,6 +5,6 @@ function saveJSON($JSONString)
     // If no permission error shows,
     // chmod 777 project folder
     $fp = fopen(ROOT . SCRIPT_DIR . "jsonWrite.json", 'w');
-    fwrite($fp, json_encode($JSONString));
+    fwrite($fp, $JSONString);
     fclose($fp);
 }
