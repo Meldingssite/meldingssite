eda182f959d554d44c2653fbb120b9c8b47188f3
Damion de Wit <damion.dewit@outlook.com>
2018-04-23 12:30:26 +0200

    Last save I forgot

--- model/HomeModel.php
+++ model/HomeModel.php
@@ -1,23 +1,11 @@
 <?php
 function sendDataModel()
 {
-    $servername = "localhost";
-//           ;
+
     $type = $_POST["type"];
     $locatie = $_POST['locatie'];
-    $username = "root";
-    $passwordDB = "mysql";
-    $database = "meldingssite";
     $tabel =  $_POST['School'];
-   // LEERPARKPROMENADE 100
 
-    $conn = new mysqli($servername, $username, $passwordDB, $database);
-    if ($conn->connect_error) {
-        $conn2 = new mysqli($servername, $username, '', $database);
-        if ($conn2->connect_error) {
-            die("Connection failed: " . $conn->connect_error . $conn->connect_error);
-        }
-    }
     $sql = "INSERT INTO `$tabel` (type, locatie)
   VALUES ('$type', '$locatie')";
     if ($conn->query($sql) === TRUE) {
