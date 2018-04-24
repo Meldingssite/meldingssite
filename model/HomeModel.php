041de77575745470150417065848928b6559e304
Damion de Wit <damion.dewit@outlook.com>
2018-04-24 10:32:34 +0200

    HomeModel.php Code Cleanup

--- model/HomeModel.php
+++ model/HomeModel.php
@@ -3,7 +3,7 @@
 function sendDataModel()
 {
     /*
-    Function Description
+    Sends Data to Database
     */
 
     //  Variable Init
@@ -12,8 +12,10 @@ function sendDataModel()
     $keys = [];
     $data = [];
     $id = $_POST['id'];
-    foreach ($_POST as $key => $ls_value) {
-        if ($key != 'School' && $key != 'id') {
+    foreach ($_POST as $key => $ls_value)
+    {
+        if ($key != 'School' && $key != 'id')
+        {
             $keys[] = $key;
             $data[] = $ls_value;
         }
@@ -22,7 +24,9 @@ function sendDataModel()
     // convert array to comma separated string
     $dataString = implode(", ", $data);
     $keyString = implode(", ", $keys);
-    if (!$id) {
+
+    if (!$id)   // No id given
+    {
         $sql = "INSERT INTO `$tabel`  ($keyString) VALUES (";
         foreach ($data as $key) {
             $sql .= '"';
@@ -31,27 +35,26 @@ function sendDataModel()
         }
         $sql = substr($sql, 0, -1);
         $sql .= ")";
-        if ($conn->query($sql)) {
 
+        if ($conn->query($sql))
             echo mysqli_insert_id($conn);
-            return mysqli_insert_id($conn);
-        } else {
+        else
             echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    } else {
-        for ($x = 0; $x < count($keys); $x++) {
-            echo $keys[$x];
-            $sql = "UPDATE `$tabel`  SET $keys[$x] = '$data[$x]' WHERE id = '$id'";
-            if ($conn->query($sql)) {
-                echo mysqli_insert_id($conn);
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        }
-        return mysqli_insert_id($conn);
-
     }
 
+    else    // id given
+    {
+        for ($x = 0; $x < count($keys); $x++)
+        {
+            echo $keys[$x];
+            $sql = "UPDATE `$tabel`  SET $keys[$x] = '$data[$x]' WHERE id = '$id'";
+            if ($conn->query($sql))
+                echo mysqli_insert_id($conn);
+            else
+                echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+    
+    return mysqli_insert_id($conn);
     $conn->close();
-    // 2 verschillende functies maken, 1e add hij de locatie en krijgt hij een id, 2e gebruikt hij deze id om hem te inserten
 }
