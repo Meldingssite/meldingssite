3a95da1c2da1b7a3c5c4940bf9ef47e7806309f7
Unknown <vincent1906@live.nl>
2018-05-23 11:41:03 +0200

    Removing some stuffs

--- model/AdminModel.php
+++ model/AdminModel.php
@@ -5,14 +5,14 @@ function saveJSON()
     // If permission denied error shows,
     // chmod 777 project folder
 
-    $fp = fopen(ROOT . SCRIPT_DIR . "pages.json", 'w');
+    $fp = fopen(ROOT . SCRIPT_DIR . "unused.json", 'w');
     fwrite($fp, $_POST["JSON"]);
     fclose($fp);
 }
 
 function deleteJSON()
 {
-    if (unlink(ROOT . SCRIPT_DIR . "pages.json"))
+    if (unlink(ROOT . SCRIPT_DIR . "unused.json"))
         echo "File Deleted.";
 }
 
