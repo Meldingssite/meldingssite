e4feeada964a839b999111ecdf19c6723034a0bf
Damion de Wit <damion.dewit@outlook.com>
2018-05-28 14:35:50 +0200

    Fixed submit shit

--- model/HomeModel.php
+++ model/HomeModel.php
@@ -11,7 +11,7 @@ function sendDataModel()
     $conn = OpenDatabaseConnection();
     $keys = [];
     $data = [];
-    $id = $_POST['id'];
+    // var_dump($_POST);
     foreach ($_POST as $key => $ls_value) {
         if ($key != 'School' && $key != 'id') {
             $keys[] = $key;
@@ -26,7 +26,7 @@ function sendDataModel()
     $dataString = implode(", ", $data);
     $keyString = implode(", ", $keys);
 
-    if (!$id) {   // No id given
+    if (!isset($_POST['id'])) {   // No id given
 
 //        var_dump($_FILES); var_dump($_POST);
         $sql = "INSERT INTO `$tabel`  ($keyString) VALUES (";
@@ -44,6 +44,7 @@ function sendDataModel()
             echo "Error: " . $sql . "<br>" . $conn->error;
     } else // id given
     {
+        $id = $_POST['id'];
         for ($x = 0;
              $x < count($keys);
              $x++) {
@@ -120,6 +121,6 @@ function sendDataModel()
     }
 
 
-    return mysqli_insert_id($conn);
+    //return mysqli_insert_id($conn);
     $conn->close();
 }
