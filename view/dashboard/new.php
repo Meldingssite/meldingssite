77dca243ad297a0ad3c8102a149969f24478427e
Damion de Wit <damion.dewit@outlook.com>
2018-05-29 16:58:43 +0200

    FInishing up style some more

--- view/dashboard/new.php
+++ view/dashboard/new.php
@@ -52,4 +52,54 @@
     </div>
 </div>
 
+<div class="alertItem">
+    <div>
+    <img    src="<?php echo URL . IMAGE_DIR ?>category-FirstAid.png"
+            alt="alert type">
+        <p class="type">Ongeval</p>
+        <p class="time">12:34</p>
+    </div>
+
+    <div style="background: url(
+        '<?php echo URL . IMAGE_DIR ?>DashboardBuildings/building-azzurro.jpg'
+    )">
+        <h1>Azzurro</h1>
+        <p>Dit is extra info over de locatie</p>
+    </div>
+
+    <div>
+    <p><ion-icon name="alert"></ion-icon> Het slachtoffer is bewusteloos</p>
+    <div class="icon-list">
+            <ion-icon name="close"></ion-icon>
+            <ion-icon name="eye"></ion-icon>
+            <ion-icon name="checkmark"></ion-icon>
+        </div>
+    </div>
+</div>
+
+<div class="alertItem">
+    <div>
+    <img    src="<?php echo URL . IMAGE_DIR ?>category-FirstAid.png"
+            alt="alert type">
+        <p class="type">Ongeval</p>
+        <p class="time">12:34</p>
+    </div>
+
+    <div style="background: url(
+        '<?php echo URL . IMAGE_DIR ?>DashboardBuildings/building-azzurro.jpg'
+    )">
+        <h1>Azzurro</h1>
+        <p>Dit is extra info over de locatie</p>
+    </div>
+
+    <div>
+    <p><ion-icon name="alert"></ion-icon> Het slachtoffer is bewusteloos</p>
+    <div class="icon-list">
+            <ion-icon name="close"></ion-icon>
+            <ion-icon name="eye"></ion-icon>
+            <ion-icon name="checkmark"></ion-icon>
+        </div>
+    </div>
+</div>
+
 <script src="https://unpkg.com/ionicons@4.1.2/dist/ionicons.js"></script>
