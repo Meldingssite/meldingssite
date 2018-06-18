9ac9b50f2188f944604fb7efdc79f81b668c6881
Unknown <vincent1906@live.nl>
2018-06-18 11:43:25 +0200

    LogOut function
    TODO Logout Button

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -2,6 +2,7 @@
 <script src="<?php echo URL . SCRIPT_DIR ?>dashboard/dashboard.js"></script>
 <header>
     <img src="<?php echo IMAGE_DIR ?>banner.jpg" alt="">
+<!--    TODO logout Button-->
 </header>
 <main id="Dashboard">
 
