237cfceca263924f6a38b3d0ba74fa488a71a767
Unknown <vincent1906@live.nl>
2018-06-19 10:14:58 +0200

    Fixes

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -2,14 +2,14 @@
 <script src="<?php echo URL . SCRIPT_DIR ?>dashboard/dashboard.js"></script>
 <header>
     <img src="<?php echo IMAGE_DIR ?>banner.jpg" alt="">
-<!--    TODO logout Button (logOut function PHP)-->
+    <!--    TODO logout Button (logOut function PHP)-->
+    <div id="Menu">
+        <button id="btnArchive">Naar Archief</button>
+        <button id="btnChangePasswords">Wachtwoorden Wijzigen</button>
+        <button id="btnLogout">Uitloggen</button>
+    </div>
 </header>
-<main id="Dashboard" onload = "start()">
+<main id="Dashboard" onload="start()">
 
 </main>
 
-<div id="Menu">
-    <button id="btnArchive">Naar Archief</button>
-    <button id="btnChangePasswords">Wachtwoorden Wijzigen</button>
-    <button id="btnLogout">Uitloggen</button>
-</div>
