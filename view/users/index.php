1be6a965e700df998a244e613106a52d74219d98
Unknown <vincent1906@live.nl>
2018-06-19 14:23:07 +0200

    Edit page

--- view/users/index.php
+++ view/users/index.php
@@ -1,9 +1,14 @@
 <link rel="stylesheet" href="<?php echo URL ?>public/css/users.css">
-<?php var_dump($data) ?>
+<?php //var_dump($data) ?>
 </header>
-<?php foreach ($data as $users): ?>
+<?php foreach ($data
+
+               as $users): ?>
     <div class="user">
-        <i class="fas fa-user-edit editEmail"></i>
+        <a href="users/edit/<?php echo $users['email'] ?> ">
+            <i class=" fas fa-user-edit editEmail">
+            </i></a>
+
         <p><?php echo $users['email'] ?></p>
         <p><?php echo $users['password'] ?></p>
     </div>
