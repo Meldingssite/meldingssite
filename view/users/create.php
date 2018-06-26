d273f3bb42d37399393b7c824e6d6bef2f6a931d
Damion de Wit <damion.dewit@outlook.com>
2018-06-26 12:12:01 +0200

    Lekker die add user styling geven boii

--- view/users/create.php
+++ view/users/create.php
@@ -1,10 +1,13 @@
-<?php
+<link rel="stylesheet" href="<?php echo URL ?>public/css/users.css">
+<link rel="stylesheet" href="<?php echo URL ?>public/css/forms.css">
+<header>
+    <h1>Nieuwe Gebruiker<h1>
+</header>
 
-?>
-<form action="addUser" method="post">
-    <h1>create User
-    </h1>
-    <input type="email" name="email" required>
-    <input type="password" name="password" required>
-    <input type="submit">
-</form>
+<main>
+    <form action="addUser" method="post">
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Wachtwoord" required>
+        <input type="submit">
+    </form>
+</main>
