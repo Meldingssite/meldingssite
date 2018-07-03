40e43bfc160df15ac81372d28f504e6959576a8d
Unknown <vincent1906@live.nl>
2018-07-03 12:07:49 +0200

    Https redirect

--- controller/DashboardController.php
+++ controller/DashboardController.php
@@ -2,6 +2,13 @@
 require(ROOT . "model/DashboardModel.php");
 session_start();
 
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}
+
 
 function index($msg = null)
 {
