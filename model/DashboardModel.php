dee3dcfe2cc218bea88b1bbeeba5eab1ed8e649c
Unknown <vincent1906@live.nl>
2018-07-03 10:21:59 +0200

    Revert "Werkend inloggen"
    This reverts commit 8568e7b509c1f421d4b9f5f75ea78e326056abb8.

--- model/DashboardModel.php
+++ model/DashboardModel.php
@@ -5,7 +5,7 @@ function retrieveElements()
     if (isset($_SESSION['username'])) {
 
         $conn = OpenDatabaseConnection();
-        $id = $conn->mysqli_real_escape_string($_POST['id']);
+        $id = $_POST['id'];
         $dataArray = [];
         $tabel = mainTable;
         $sql = "select * from `$tabel` where id=$id";
@@ -51,14 +51,13 @@ function deleteEntry()
 {
     if (isset($_SESSION['username'])) {
         $tabel = mainTable;
-
+        $id = $_POST['id'];
         // Create connection
         $conn = OpenDatabaseConnection();
         // Check connection
         if ($conn->connect_error)
             die("Connection failed: " . $conn->connect_error);
 
-        $id = $conn->mysqli_real_escape_string($_POST['id']);
         $sql = "select * from `$tabel` where id=$id";
         $result = $conn->query($sql);
         $data = $result->fetch_assoc();
@@ -88,7 +87,7 @@ function setCompleted()
         $tabel = mainTable;
         // Create connection
         $conn = OpenDatabaseConnection();
-        $id = $conn->mysqli_real_escape_string($_POST['id']);
+        $id = $_POST['id'];
         // Check connection
         if ($conn->connect_error)
             die("Connection failed: " . $conn->connect_error);
@@ -184,7 +183,9 @@ function loginValid($emailTemp, $passTemp)
                     return $out;
                 } else {
                     // Display an error message if password is not valid
-                    $password_err = 'The password you entered was not valid.';
+                    $out['success'] = FALSE;
+                    $out['error'] = 2;
+                    return $out;
                 }
             }
         } else {
@@ -203,7 +204,7 @@ function loginValid($emailTemp, $passTemp)
         echo "Error: " . $conn->error . "\n";
 
         $out['success'] = FALSE;
-        $out['error'] = '3';
+        $out['error'] = 3;
         return $out;
 //        echo "Oops! Something went wrong. Please try again later.";
 
