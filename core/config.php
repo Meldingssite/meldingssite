f751de263ee8a8ac03292a7497ff3d470df893c8
Unknown <vincent1906@live.nl>
2018-09-06 11:50:30 +0200

    beep

--- core/config.php
+++ core/config.php
@@ -5,7 +5,7 @@
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'meldendvc_nl_meldingsite');
+define('DB_USER', 'melding_site');
 define('DB_PASS', 'mysql');
 define('DB_NAME', 'meldendvc_nl_meldingsite');
 define('salt', 'csgodevonREEEEEEEEEEEEEEE');
